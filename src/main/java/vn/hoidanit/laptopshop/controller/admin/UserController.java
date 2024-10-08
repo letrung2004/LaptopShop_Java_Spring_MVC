19c2f4dbf2ff71dd66686b87d404a1c72e7312f4
le trung <lequoctrunggg@gmail.com>
2024-10-08 17:32:40 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UploadService;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class UserController {
@@ -31,15 +35,6 @@ public class UserController {
     }
 
     // ----dependency injection block----
-
-    // @GetMapping("/")
-    // public String getHomePage(Model model) {
-    // model.addAttribute("trung", "test");
-    // model.addAttribute("javaspring", "from controller with model");
-    // return "test";// cái return này là trả lại đường dẫn của file trong code con
-    // đường dẫn ở trên
-    // // mới là đường dẫn trên localhot
-    // }
 
     // trang table user
     @GetMapping("/admin/user")
@@ -59,8 +54,17 @@ public class UserController {
     // submit trang create User
     @PostMapping("/admin/user/create")
     public String createUserPage(Model model,
-            @ModelAttribute("newUser") User hoidanit,
+            @ModelAttribute("newUser") @Valid User hoidanit, BindingResult newUserBindingResult,
             @RequestParam("hoidanitFile") MultipartFile file) {
+
+        List<FieldError> errors = newUserBindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(" ---> " + error.getField() + " - " + error.getDefaultMessage());
+        }
+        // validate
+        if (newUserBindingResult.hasErrors()) {
+            return "admin/user/create";
+        }
         String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
         String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
         hoidanit.setAvatar(avatar);
